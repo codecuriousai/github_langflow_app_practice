0767acfdf39666576171799d67c202ee1c7429b9
codecuriousai <codecuriousai@gmail.com>
2025-09-03 07:43:11 +0530

    added code for login functionality

--- login.js
+++ login.js
@@ -26,3 +26,32 @@ try {
 }
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// Additional Sonar issues below (no comments)
+
+function deprecatedFunction() {
+    arguments.callee(); // Use of deprecated 'arguments.callee'
+}
+
+let x;
+if (x == null) {
+    x = 5;
+}
+
+function insecureRandom() {
+    return Math.random(); // Use of insecure random
+}
+
+function unusedFunction() {
+    return true;
+}
+
+let arr = [1,2,3];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] = 2) { // Assignment in conditional
+        break;
+    }
+}
+
+let obj = {};
+delete obj.nonexistentProperty; // Deleting a non-existent property
