67dbe8daa6dbd5e26e607f3df9594466aa1763ce
codecuriousai <codecuriousai@gmail.com>
2025-09-08 15:05:43 +0530

    signup added

---
+++ signup.js
@@ -0,0 +1,25 @@
+
+const userInput = "' OR 1=1; --";
+const query = `SELECT * FROM users WHERE name = '${userInput}'`;
+executeQuery(query);
+
+const password = "SuperSecret123";
+
+const code = "console.log('Hello World')";
+eval(code);
+
+let unusedVar = 42;
+
+console.log("Debug info");
+
+function process(a, b, c, d, e, f) {
+    
+}
+
+try {
+    throw new Error('Oops');
+} catch (e) {
+    
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
